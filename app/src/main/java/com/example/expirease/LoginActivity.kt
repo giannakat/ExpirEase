426541b80cb118c9523f92470d3d11acb876cf06
giannakat <gkcarreon@gmail.com>
2025-02-19 01:27:40 +0800

    added login and signup transition

--- app/src/main/java/com/example/expirease/LoginActivity.kt
+++ app/src/main/java/com/example/expirease/LoginActivity.kt
@@ -1,12 +1,50 @@
 package com.example.expirease
 
 import android.app.Activity
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.view.View
+import android.widget.TextView
 
 class LoginActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
+
+        val signUpTextView: TextView = findViewById(R.id.signUpTextView)
+
+        // Create a SpannableString to make "Sign up" a different color and clickable
+        val text = "Don't have an Account? Sign up"
+        val spannableString = SpannableString(text)
+
+        // Change the color of the "Sign up" text to blue (or any color you prefer)
+        val startIndex = text.indexOf("Sign up")
+        val endIndex = startIndex + "Sign up".length
+        spannableString.setSpan(ForegroundColorSpan(Color.GREEN), startIndex, endIndex, 0)
+
+        // Make "Sign up" clickable
+        spannableString.setSpan(object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                // Navigate to Sign Up Activity when clicked
+                val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                startActivity(intent)
+            }
+        }, startIndex, endIndex, 0)
+
+        // Set the SpannableString to the TextView
+        signUpTextView.text = spannableString
+
+        // Make the TextView clickable
+        signUpTextView.movementMethod = LinkMovementMethod.getInstance()
+
 
     }
 }
