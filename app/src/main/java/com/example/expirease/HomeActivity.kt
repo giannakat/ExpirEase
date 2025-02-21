8ae403a7d0f258bd2281b7ba235e309e62fab59c
giannakat <gkcarreon@gmail.com>
2025-02-21 23:48:13 +0800

    cleaned main

--- app/src/main/java/com/example/expirease/HomeActivity.kt
+++ app/src/main/java/com/example/expirease/HomeActivity.kt
@@ -12,37 +12,9 @@ class HomeActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_landing)
 
-        // Get references
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-
-        toolbar.title = "ExpirEase"
-
-        // Set Toolbar as ActionBar
-        setActionBar(toolbar)
-
-        // Enable home button and set custom icon
-        actionBar?.let {
-            it.setDisplayHomeAsUpEnabled(true)
-            it.setHomeAsUpIndicator(R.drawable.burger_menu) // Custom icon in res/drawable
-        }
-
-        // Handle Toolbar navigation click
-        toolbar.setNavigationOnClickListener {
-            drawerLayout.openDrawer(GravityCompat.START)
-        }
     }
 
-    // Handle menu item clicks
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        if (item.itemId == android.R.id.home) {
-            drawerLayout.openDrawer(GravityCompat.START)
-            return true
-        }
-        return super.onOptionsItemSelected(item)
     }
 }
