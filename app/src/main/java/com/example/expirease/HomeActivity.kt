319d67781535979a9f7c69b7c028a4e044bd6d0e
giannakat <gkcarreon@gmail.com>
2025-02-22 20:59:36 +0800

    added button functionalities

---
+++ app/src/main/java/com/example/expirease/HomeActivity.kt
@@ -0,0 +1,32 @@
+package com.example.expirease
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+
+        val menuButton: ImageView = findViewById(R.id.menu_button)
+
+
+
+        menuButton.setOnClickListener {
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+}
