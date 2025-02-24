da29d93394b1af5c0562a5f13098a732c18b142a
JMAC137 <51169542+JonMariC137@users.noreply.github.com>
2025-02-24 19:27:50 +0800

    added transition to pages

--- app/src/main/java/com/example/expirease/MenuActivity.kt
+++ app/src/main/java/com/example/expirease/MenuActivity.kt
@@ -1,7 +1,9 @@
 package com.example.expirease
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +13,22 @@ class MenuActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navigation)
+
+        val homeButton: ImageView = findViewById(R.id.home_button)
+        val settingsButton: ImageView = findViewById(R.id.settings_button)
+        val logoutButton: ImageView = findViewById(R.id.logout_button)
+
+        homeButton.setOnClickListener {
+            val intent = Intent(this@MenuActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+        logoutButton.setOnClickListener {
+            val intent = Intent(this, LogoutActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
