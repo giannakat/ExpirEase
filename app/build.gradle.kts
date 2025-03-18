c615ea809def82daab4411bb1183015029a1208c
giannakat <gkcarreon@gmail.com>
2025-03-19 01:25:07 +0800

    fixed appcompat issue
    there's still some undefined behavior with navigation but it works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,8 +73,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     //navigation
-//    implementation(libs.material)
-//    implementation(libs.androidx.appcompat)
-    implementation 'androidx.appcompat:appcompat:1.6.1'
-    implementation 'com.google.android.material:material:1.10.0'
-}
+    implementation(libs.material)
+    implementation(libs.androidx.appcompat)
+   }
